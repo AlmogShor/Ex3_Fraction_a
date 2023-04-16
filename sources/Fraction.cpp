b1a47d5ab2ce33c7337b5c44e03113c6ad264c41
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-04-16 13:24:21 +0300

    upload git ignore plus minors changees

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -21,13 +21,13 @@ Fraction::Fraction(int numerator, int denominator) {
     }
     this->numerator = numerator;
     this->denominator = denominator;
-    reduce();
+    this->reduce();
 }
 
 Fraction::Fraction(double value) {
     this->numerator = value * 1000;
     this->denominator = 1000;
-    reduce();
+    this->reduce();
 }
 
 /**
@@ -37,18 +37,21 @@ Fraction::Fraction(double value) {
 Fraction Fraction::operator+(const Fraction &other) const {
     int newNumerator = this->numerator * other.denominator + other.numerator * this->denominator;
     int newDenominator = this->denominator * other.denominator;
+    this->reduce();
     return Fraction(newNumerator, newDenominator);
 }
 
 Fraction Fraction::operator-(const Fraction &other) const {
     int newNumerator = this->numerator * other.denominator - other.numerator * this->denominator;
     int newDenominator = this->denominator * other.denominator;
+    this->reduce();
     return Fraction(newNumerator, newDenominator);
 }
 
 Fraction Fraction::operator*(const Fraction &other) const {
     int newNumerator = this->numerator * other.numerator;
     int newDenominator = this->denominator * other.denominator;
+    this->reduce();
     return Fraction(newNumerator, newDenominator);
 }
 
@@ -59,6 +62,63 @@ Fraction Fraction::operator/(const Fraction &other) const {
     }
     int newNumerator = this->numerator * other.denominator;
     int newDenominator = this->denominator * other.numerator;
+    this->reduce();
     return Fraction(newNumerator, newDenominator);
 }
+
+
+/**
+ * Arithmetic operators with double on the left
+ */
+
+/**
+ * Arithmetic operators with double on the right
+ */
+
+/**
+ * Comparison operators
+ */
+
+
+/**
+ * Comparison operators with double
+ */
+
+/**
+ * Increment and decrement operators
+ */
+
+Fraction &Fraction::operator++() {
+    this->numerator += this->denominator;
+    this->reduce();
+    return *this;
+}
+
+Fraction &Fraction::operator--() {
+    this->numerator -= this->denominator;
+    this->reduce();
+    return *this;
+}
+
+/**
+ * Reduction functions
+ */
+ void Fraction::reduce() {// need to fix
+    int gcd = __gcd(this->numerator, this->denominator);
+    this->numerator /= gcd;
+    this->denominator /= gcd;
+    if (this->denominator < 0) {
+        this->numerator *= -1;
+        this->denominator *= -1;
+    }
+}
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+
 
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -16,26 +16,28 @@ namespace ariel {
         Fraction(double value);                 // Constructor with value as a float
 
         // Arithmetic operators
-        Fraction operator+(const Fraction& other) const;  // Addition
-        Fraction operator-(const Fraction& other) const;  // Subtraction
-        Fraction operator*(const Fraction& other) const;  // Multiplication
-        Fraction operator/(const Fraction& other) const;  // Division
+        Fraction operator+(const Fraction &other) const;  // Addition
+        Fraction operator-(const Fraction &other) const;  // Subtraction
+        Fraction operator*(const Fraction &other) const;  // Multiplication
+        Fraction operator/(const Fraction &other) const;  // Division
         Fraction operator+(double value) const;           // Addition with a float value from the left
         Fraction operator-(double value) const;           // Subtraction with a float value from the left
         Fraction operator*(double value) const;           // Multiplication with a float value from the left
         Fraction operator/(double value) const;           // Division with a float value from the left
 
-        friend Fraction operator+(double value, const Fraction& fraction); //Addition with a float value from the right
-        friend Fraction operator-(double value, const Fraction& fraction); //Subtraction with a float value from the right
-        friend Fraction operator*(double value, const Fraction& fraction); //Multiplication with a float value from the right
-        friend Fraction operator/(double value, const Fraction& fraction); //Division with a float value from the right
+        friend Fraction operator+(double value, const Fraction &fraction); //Addition with a float value from the right
+        friend Fraction
+        operator-(double value, const Fraction &fraction); //Subtraction with a float value from the right
+        friend Fraction
+        operator*(double value, const Fraction &fraction); //Multiplication with a float value from the right
+        friend Fraction operator/(double value, const Fraction &fraction); //Division with a float value from the right
         // Comparison operators
-        bool operator==(const Fraction& other) const;  // Equality
-        bool operator!=(const Fraction& other) const;  // Inequality
-        bool operator>(const Fraction& other) const;   // Greater than
-        bool operator<(const Fraction& other) const;   // Less than
-        bool operator>=(const Fraction& other) const;  // Greater than or equal to
-        bool operator<=(const Fraction& other) const;  // Less than or equal to
+        bool operator==(const Fraction &other) const;  // Equality
+        bool operator!=(const Fraction &other) const;  // Inequality
+        bool operator>(const Fraction &other) const;   // Greater than
+        bool operator<(const Fraction &other) const;   // Less than
+        bool operator>=(const Fraction &other) const;  // Greater than or equal to
+        bool operator<=(const Fraction &other) const;  // Less than or equal to
         bool operator==(double value) const;           // Equality with a float value
         bool operator!=(double value) const;           // Inequality with a float value
         bool operator>(double value) const;            // Greater than a float value
@@ -44,21 +46,23 @@ namespace ariel {
         bool operator<=(double value) const;           // Less than or equal to a float value
 
         // Increment and decrement operators
-        Fraction& operator++();    // Prefix increment
+        Fraction &operator++();    // Prefix increment
         Fraction operator++(int);  // Postfix increment
-        Fraction& operator--();    // Prefix decrement
+        Fraction &operator--();    // Prefix decrement
         Fraction operator--(int);  // Postfix decrement
 
         // Input and output operators
-        friend std::ostream& operator<<(std::ostream& os, const Fraction& fraction);  // Output
-        friend std::istream& operator>>(std::istream& is, Fraction& fraction);        // Input
+        friend std::ostream &operator<<(std::ostream &os, const Fraction &fraction);  // Output
+        friend std::istream &operator>>(std::istream &is, Fraction &fraction);        // Input
 
     private:
         int numerator;   // Numerator of the fraction
         int denominator; // Denominator of the fraction
 
-        // Helper function to reduce the fraction to its simplest form
+        // Helper functions to reduce the fraction to its simplest form
         void reduce();
+
+        int gcd(int a, int b);
     };
 
 } // namespace ariel
